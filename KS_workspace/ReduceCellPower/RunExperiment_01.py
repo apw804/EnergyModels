5fec318df70d4152a129f8acf9e3782e11e6cd00
Kishan Sthankiya <apw804@qmul.ac.uk>
2023-01-26 18:19:55 +0000

    Added code for progressbars

--- KS_workspace/ReduceCellPower/RunExperiment_01.py
+++ KS_workspace/ReduceCellPower/RunExperiment_01.py
@@ -1,43 +1,48 @@
-# Script for multiple runs of ReduceCellPower12.py
+# Script for multiple runs of ReduceCellPower13.py
 # Use as a template.
 # Kishan Sthankiya
 # Combine seed number and power_dBm range
 
 import argparse
 import numpy as np
-import itertools
-
-from ReduceCellPower_12 import test_01
+from progress.bar import IncrementalBar, Bar
+from ReduceCellPower_13 import test_01
 
 # Set the Experiment name
 name = "Exp_Seeds"
 
 # Define the seeds range
-seeds_list = list(range(1000))
+seeds_list = list(range(10))
 
 # Define power_dBm range
 power_dBm_list = list((range(30, 50)))
 power_dBm_list_floats = [float(q) for q in power_dBm_list]
 
 if __name__ == '__main__':
-    for i in seeds_list:
-        for j in power_dBm_list:
-            np.set_printoptions(precision=4, linewidth=200)
-            parser = argparse.ArgumentParser()
-            parser.add_argument('-seed', type=int, default=i, help='seed value for random number generator')
-            parser.add_argument('-isd', type=float, default=500.0, help='Base station inter-site distance in metres')
-            parser.add_argument('-sim_radius', type=float, default=1000.0, help='Simulation bounds radius in metres')
-            parser.add_argument('-nues', type=int, default=10, help='number of UEs')
-            parser.add_argument('-subbands', type=int, default=1, help='number of subbands')
-            parser.add_argument('-power_dBm', type=float, default=j,
-                                help='set the transmit power of the cell in dBm')
-            parser.add_argument('-until', type=float, default=2.0, help='simulation time')
-            parser.add_argument('-logging_interval', type=float, default=1.0,
-                                help='Sampling interval (seconds) for simulation data capture + UEs reports sending.')
-            parser.add_argument('-experiment_name', type=str, default=None,
-                                help='name of a specific experiment to influence the output log names.')
-            args = parser.parse_args()
+    with IncrementalBar('Processing seeds:', max=len(seeds_list)) as seed_bar:
+        for i in seeds_list:
+            with Bar('Processing power_dBm:', max=len(power_dBm_list_floats)) as power_bar:
+                for j in power_dBm_list_floats:
+                    np.set_printoptions(precision=4, linewidth=200)
+                    parser = argparse.ArgumentParser()
+                    parser.add_argument('-seed', type=int, default=i, help='seed value for random number generator')
+                    parser.add_argument('-isd', type=float, default=500.0,
+                                        help='Base station inter-site distance in metres')
+                    parser.add_argument('-sim_radius', type=float, default=1000.0,
+                                        help='Simulation bounds radius in metres')
+                    parser.add_argument('-nues', type=int, default=1000, help='number of UEs')
+                    parser.add_argument('-subbands', type=int, default=1, help='number of subbands')
+                    parser.add_argument('-power_dBm', type=float, default=j,
+                                        help='set the transmit power of the cell in dBm')
+                    parser.add_argument('-until', type=float, default=2.0, help='simulation time')
+                    parser.add_argument('-logging_interval', type=float, default=1.0,
+                                        help='Sampling interval (seconds) for simulation data capture + UEs reports sending.')
+                    parser.add_argument('-experiment_name', type=str, default=None,
+                                        help='name of a specific experiment to influence the output log names.')
+                    args = parser.parse_args()
 
-            test_01(seed=args.seed, subbands=args.subbands, isd=args.isd, sim_radius=args.sim_radius,
-                    power_dBm=args.power_dBm, nues=args.nues, until=args.until, sim_args_dict=args.__dict__,
-                    logging_interval=args.logging_interval, experiment_name=name)
+                    test_01(seed=args.seed, subbands=args.subbands, isd=args.isd, sim_radius=args.sim_radius,
+                            power_dBm=args.power_dBm, nues=args.nues, until=args.until, sim_args_dict=args.__dict__,
+                            logging_interval=args.logging_interval, experiment_name=name)
+                    power_bar.next()
+                seed_bar.next()
