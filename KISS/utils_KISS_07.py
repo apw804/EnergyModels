cd9066153a67abfe212183c4449061af1cee704e
kishansthankiya <101208911+apw804@users.noreply.github.com>
2023-03-13 10:17:48 +0000

    Updated logfile handling, multiprocessing logic

--- KISS/utils_KISS_07.py
+++ KISS/utils_KISS_07.py
@@ -1,5 +1,21 @@
 from pandas import Timestamp
 
-def get_timestamp() -> str:
-    """Return the current timestamp in ISO 8601 format."""
-    return Timestamp.now(tz=None).isoformat(timespec='seconds')
+from typing import Optional
+
+def get_timestamp(date_only: Optional[bool] = False, time_only: Optional[bool] = False) -> str:
+    """Return the current timestamp in ISO 8601 format.
+
+    Args:
+        date_only (bool): If True, return only the date in YYYY-MM-DD format.
+        time_only (bool): If True, return only the time in HHMMSS format.
+
+    Returns:
+        str: The current timestamp.
+    """
+    if date_only:
+        return Timestamp.now(tz=None).strftime('%Y-%m-%d')
+    elif time_only:
+        return Timestamp.now(tz=None).strftime('%H%M%S')
+    else:
+        return Timestamp.now(tz=None).isoformat(timespec='seconds')
+
