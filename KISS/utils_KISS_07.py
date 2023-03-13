858ba840cde9fee648eb96e86197f7beef395cc1
kishansthankiya <101208911+apw804@users.noreply.github.com>
2023-03-13 12:48:50 +0000

    Added argument to generate seed value from timestamp

--- KISS/utils_KISS_07.py
+++ KISS/utils_KISS_07.py
@@ -1,13 +1,13 @@
 from pandas import Timestamp
-
 from typing import Optional
 
-def get_timestamp(date_only: Optional[bool] = False, time_only: Optional[bool] = False) -> str:
+def get_timestamp(date_only: Optional[bool] = False, time_only: Optional[bool] = False, for_seed: Optional[bool] = False) -> str:
     """Return the current timestamp in ISO 8601 format.
 
     Args:
         date_only (bool): If True, return only the date in YYYY-MM-DD format.
         time_only (bool): If True, return only the time in HHMMSS format.
+        for_seed (bool): If True, return the timestamp as a seed value in the format YYYYMMDDHHMMSS.
 
     Returns:
         str: The current timestamp.
@@ -16,6 +16,7 @@ def get_timestamp(date_only: Optional[bool] = False, time_only: Optional[bool] =
         return Timestamp.now(tz=None).strftime('%Y-%m-%d')
     elif time_only:
         return Timestamp.now(tz=None).strftime('%H%M%S')
+    elif for_seed:
+        return Timestamp.now(tz=None).strftime('%Y%m%d%H%M%S')
     else:
         return Timestamp.now(tz=None).isoformat(timespec='seconds')
-
