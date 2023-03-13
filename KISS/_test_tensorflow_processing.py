3e30e38b799f8b45a71c96f7f45a1f4dfcf5bd1d
kishansthankiya <101208911+apw804@users.noreply.github.com>
2023-03-13 11:58:49 +0000

    Added tensorflow test_scripts

--- KISS/_test_tensorflow_processing.py
+++ KISS/_test_tensorflow_processing.py
@@ -1,24 +1,10 @@
 import tensorflow as tf
-import multiprocessing
 
-def process_data(data):
-    # your data processing logic here
-    print(data)
-    pass
+# create a TensorFlow operation that uses a GPU
+with tf.device('/GPU:0'):
+    a = tf.constant([1.0, 2.0, 3.0])
+    b = tf.constant([4.0, 5.0, 6.0])
+    c = a * b
 
-if __name__ == '__main__':
-    data = list(range(123))  # your data here
-    max_workers = 4  # set the maximum number of processes to use
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        # if there are GPUs available, set memory growth and use the first GPU
-        try:
-            tf.config.experimental.set_memory_growth(gpus[0], True)
-            tf.config.set_visible_devices(gpus[0], 'GPU')
-        except RuntimeError as e:
-            print(e)
-    with multiprocessing.Pool(max_workers) as pool:
-        # map each task to a separate process
-        results = pool.map_async(process_data, data)
-        # wait for all processes to finish and collect the results
-        results.wait()
+# run the operation and print the result
+print(c)
