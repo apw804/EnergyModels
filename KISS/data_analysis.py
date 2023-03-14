a5cdf109bf66d3d3b23f10404c57b08112378512
kishansthankiya <101208911+apw804@users.noreply.github.com>
2023-03-14 16:35:21 +0000

    Update data_analysis.py

--- KISS/data_analysis.py
+++ KISS/data_analysis.py
@@ -2,16 +2,17 @@ import argparse
 from pathlib import Path
 import pandas as pd
 
-from logging_KISS_07 import get_logger
+from logging_kiss import get_logger
+from process_kiss import write_dataframe
 
 # Get current script name and current working directory
 script_name = Path(__file__).stem
 cwd = Path.cwd()
 
 # Create a logger object
-logger = get_logger(script_name, cwd)
+logger = get_logger(logger_name=script_name, log_dir=cwd)
 
-def analyze_data(data_file):
+def analyze_data(data_file, show_results=True):
     # Read in the data from the feather file
     logger.info(f'Reading data from {data_file}...')
     df = pd.read_feather(data_file)
@@ -35,12 +36,31 @@ def analyze_data(data_file):
     stats_df = pd.concat(results, axis=1)
 
     logger.info('Data analysis complete.')
+
+    # Print the results
+    if show_results:
+        print(stats_df)
+    
+    # Return the results
     return stats_df
 
 
-def main(data_file_path):
+def main(data_file_path, outfile_path=None):
+
     # Call the analyze_data function with the specified data file
-    analyze_data(data_file_path)
+    summary_stats = analyze_data(data_file_path)
+
+    # Save the results to a tsv file
+    logger.info('Saving results to file...')
+    path=Path(data_file_path).parent
+    outfile_name=str(Path(data_file_path).stem) + '_summary_stats'
+    write_dataframe(
+          df=summary_stats,
+          logger=logger,
+          path=outfile_path, 
+          outfile=outfile_name,
+            file_type='tsv'
+        )
 
 
 if __name__ == '__main__':
@@ -50,9 +70,15 @@ if __name__ == '__main__':
         '--data_file_path', 
         type=str, 
         help='Path to feather file',
-        default='/Users/apw804/dev-02/EnergyModels/KISS/KISS_07_results/KISS_07_results_2023-03-09T22:22:03.feather'
-        )
+        default='KISS/_test/data/input/data_analysis/test_s100_p43dBm.fea'
+    )
+    parser.add_argument(
+        '--outfile_path',
+        type=str,
+        help='Path to output file',
+        default='KISS/_test/data/output/data_analysis'
+    )
     args = parser.parse_args()
 
     # Call the main function
-    main(data_file_path=args.data_file_path)
+    main(data_file_path=args.data_file_path, outfile_path=args.outfile_path)
