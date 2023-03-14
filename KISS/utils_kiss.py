a1efba2f9ae48591b8fa6dfee4c024d1f9079e48
kishansthankiya <101208911+apw804@users.noreply.github.com>
2023-03-14 14:01:25 +0000

    Update utils_kiss.py
    Added path validation function `validate_str_path`

--- KISS/utils_kiss.py
+++ KISS/utils_kiss.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pandas import Timestamp
 from typing import Optional
 
@@ -20,3 +21,23 @@ def get_timestamp(date_only: Optional[bool] = False, time_only: Optional[bool] =
         return int(Timestamp.now(tz=None).strftime('%H%M%S'))
     else:
         return Timestamp.now(tz=None).isoformat(timespec='seconds')
+
+def validate_str_path(directory: str) -> str:
+    # Accepts a path as a string or pathlib.Path object. Checks if it is a valid path and returns a pathlib.Path object.
+
+    # If the directory is a string, convert it to a Path object
+    if isinstance(directory, str):
+        # If the directory is a string, convert it to a Path object
+        directory = Path(directory)
+        # check if the directory is a relative path
+        if not directory.is_absolute():
+            # If it is a relative path, make it an absolute path
+            directory = directory.resolve()
+
+    # Check that the directory exists
+    if directory.resolve() is None:
+        # If the directory does not exist, return False
+        return False
+    else:
+        # If the directory does exist, return the directory as a Path object
+        return directory
